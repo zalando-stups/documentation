31f1267906d4d7caaee5047a3d2d28e04055a01e
Henning Jacobs <henning@jacobs1.de>
2015-06-15 17:55:51 +0200

    more words

---
+++ generate-word-cloud.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+'''
+make text
+find _build/text/ -name '*.txt' | xargs cat > _build/words.txt
+'''
+
+from os import path
+from wordcloud import WordCloud, STOPWORDS
+
+d = path.dirname(__file__)
+
+STOPWORDS.add('will')
+
+# Read the whole text.
+text = open(path.join(d, '_build/words.txt'), encoding='utf-8').read()
+wordcloud = WordCloud(width=1920, height=1080, max_words=500).generate(text)
+wordcloud.to_file('word-cloud.png')
