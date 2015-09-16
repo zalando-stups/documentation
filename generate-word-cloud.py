a6d8247fe3c894f948f882d86991a0cd6bf3566d
Henning Jacobs <henning@jacobs1.de>
2015-09-16 12:09:44 +0200

    generate word cloud

--- generate-word-cloud.py
+++ generate-word-cloud.py
@@ -11,8 +11,9 @@ from wordcloud import WordCloud, STOPWORDS
 d = path.dirname(__file__)
 
 STOPWORDS.add('will')
+STOPWORDS.add('example')
 
 # Read the whole text.
 text = open(path.join(d, '_build/words.txt'), encoding='utf-8').read()
-wordcloud = WordCloud(width=1920, height=1080, max_words=500).generate(text)
+wordcloud = WordCloud(width=1920, height=1080, max_words=200).generate(text)
 wordcloud.to_file('word-cloud.png')
